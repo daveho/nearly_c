beec30580e5fa8eaeee4802cf2dbc4165c44ae28
David Hovemeyer <david.hovemeyer@gmail.com>
2022-09-28 06:12:26 -0400

    generate ast.cpp, ASTVisitor class from ast.h

--- Makefile
+++ Makefile
@@ -1,23 +1,28 @@
 CXX = g++
 CXXFLAGS = -g -Wall -std=c++17 -I.
 
-GENERATED_SRCS = parse.tab.cpp lex.yy.cpp grammar_symbols.cpp
+GENERATED_SRCS = parse.tab.cpp lex.yy.cpp grammar_symbols.cpp \
+	ast.cpp ast_visitor.cpp
+GENERATED_HDRS = parse.tab.h lex.yy.h grammar_symbols.h ast_visitor.h
 SRCS = node.cpp node_base.cpp location.cpp treeprint.cpp \
-	main.cpp ast.cpp context.cpp \
-	yyerror.cpp exceptions.cpp cpputil.cpp $(GENERATED_SRCS)
+	main.cpp context.cpp \
+	yyerror.cpp exceptions.cpp cpputil.cpp \
+	$(GENERATED_SRCS)
 OBJS = $(SRCS:%.cpp=%.o)
+
+EXE = nearly_c
 
 # Uncomment one of the following depending on whether you
 # want the parser to build a parse tree or build an AST
-PARSER_SRC = parse.y
-#PARSER_SRC = parse_buildast.y
+#PARSER_SRC = parse.y
+PARSER_SRC = parse_buildast.y
 
 %.o : %.cpp
 	$(CXX) $(CXXFLAGS) -c $*.cpp -o $*.o
 
-all : nearly_c
+all : $(EXE)
 
-nearly_c : parse.tab.h lex.yy.h grammar_symbols.h grammar_symbols.cpp $(OBJS)
+$(EXE) : $(GENERATED_SRCS) $(GENERATED_HDRS) $(OBJS)
 	$(CXX) -o $@ $(OBJS)
 
 parse.tab.h parse.tab.cpp : $(PARSER_SRC)
@@ -26,18 +31,20 @@ parse.tab.h parse.tab.cpp : $(PARSER_SRC)
 lex.yy.cpp lex.yy.h : lex.l
 	flex --outfile=lex.yy.cpp --header-file=lex.yy.h lex.l
 
-grammar_symbols.h grammar_symbols.cpp : $(PARSER_SRC)
+grammar_symbols.h grammar_symbols.cpp : $(PARSER_SRC) scan_grammar_symbols.rb
 	./scan_grammar_symbols.rb < $(PARSER_SRC)
 
-depend : parse.tab.h lex.yy.h grammar_symbols.h
+ast.cpp ast_visitor.h ast_visitor.cpp : ast.h gen_ast_code.rb
+	./gen_ast_code.rb < ast.h
+
+depend : $(GENERATED_SRCS)
 	$(CXX) $(CXXFLAGS) -M $(SRCS) > depend.mak
 
 depend.mak :
 	touch $@
 
 clean :
-	rm -f *.o depend.mak \
-		$(GENERATED_SRCS) $(GENERATED_SRCS:%.cpp=%.h) \
-		-f parse.output nearly_c
+	rm -f *.o depend.mak $(GENERATED_SRCS) $(GENERATED_HDRS) \
+		parse.output $(EXE)
 
 include depend.mak
---
+++ gen_ast_code.rb
@@ -0,0 +1,140 @@
+#! /usr/bin/env ruby
+
+# This script reads ast.h and generates:
+#
+#   ast_visitor.h
+#   ast_visitor.cpp
+#   ast.cpp
+
+def visit_function_name(tag)
+  raise "huh?" if !tag.start_with?('AST_')
+  tag = tag[4..-1]
+  return "visit_#{tag.downcase}"
+end
+
+def gen_ast_visitor_h(outf, ast_tags)
+
+  outf.print <<"EOF1"
+#ifndef AST_VISITOR_H
+#define AST_VISITOR_H
+
+class ASTVisitor {
+public:
+  ASTVisitor();
+  virtual ~ASTVisitor();
+
+  virtual void visit(Node *n);
+EOF1
+
+  ast_tags.each do |tag|
+    outf.puts "  virtual void #{visit_function_name(tag)}(Node *n);"
+  end
+
+  outf.print <<"EOF2"
+  virtual void visit_children(Node *n);
+};
+
+#endif // AST_VISITOR_H
+EOF2
+end
+
+def gen_ast_visitor_cpp(outf, ast_tags)
+  outf.print <<"EOF3"
+#include "node.h"
+#include "exceptions.h"
+#include "ast.h"
+#include "ast_visitor.h"
+
+ASTVisitor::ASTVisitor() {
+}
+
+ASTVisitor::~ASTVisitor() {
+}
+EOF3
+
+  ast_tags.each do |tag|
+    outf.print <<"EOF4"
+
+void ASTVisitor::#{visit_function_name(tag)}(Node *n) {
+  visit_children(n);
+}
+EOF4
+  end
+
+  outf.print <<"EOF5"
+
+void ASTVisitor::visit(Node *n) {
+  switch (n->get_tag()) {
+EOF5
+
+  ast_tags.each do |tag|
+    outf.puts "  case #{tag}:"
+    outf.puts "    #{visit_function_name(tag)}(n); break;"
+  end
+
+  outf.print <<"EOF6"
+  default:
+    RuntimeError::raise("Unknown AST node tag %d", n->get_tag());
+  }
+}
+
+void ASTVisitor::visit_children(Node *n) {
+  for (auto i = n->cbegin(); i != n->cend(); ++i) {
+    visit(*i);
+  }
+}
+EOF6
+end
+
+def gen_ast_cpp(outf, ast_tags)
+  outf.print <<"EOF7"
+#include <cassert>
+#include "node.h"
+#include "ast.h"
+
+ASTTreePrint::ASTTreePrint() {
+}
+
+ASTTreePrint::~ASTTreePrint() {
+}
+
+std::string ASTTreePrint::node_tag_to_string(int tag) const {
+  switch (tag) {
+EOF7
+
+  ast_tags.each do |tag|
+    outf.puts "  case #{tag}: return \"#{tag}\";"
+  end
+
+  outf.print <<"EOF8"
+    // If the tag doesn't match any of the AST node tags,
+    // assume it's a parse tree node
+  default: return ParseTreePrint::node_tag_to_string(tag);
+  }
+}
+EOF8
+end
+
+ast_tags = []
+
+STDIN.each_line do |line|
+  line.rstrip!
+
+  if m = /^\s+(AST_\S+)\s*(=\s*\d+\s*)?,(\s*\/\/.*)?$/.match(line)
+    ast_tags.push(m[1])
+  end
+end
+
+#puts "found #{ast_tags.length} AST tags"
+
+File.open('ast_visitor.h', 'w') do |outf|
+  gen_ast_visitor_h(outf, ast_tags);
+end
+
+File.open('ast_visitor.cpp', 'w') do |outf|
+  gen_ast_visitor_cpp(outf, ast_tags)
+end
+
+File.open('ast.cpp', 'w') do |outf|
+  gen_ast_cpp(outf, ast_tags)
+end
